9242cee1703d854bb4b53e885c166caee8485e16
Mariam arkiza <arkizamariam96@gmail.com>
2025-09-29 22:00:26 +0100

    Exercise1.3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -7,20 +7,28 @@ const Header = (props) => {
   )
 }
 
-
+const Part = (props) => {
+  return(
+    <>
+      <p>
+        {props.part} {props.exercise}
+      </p>
+    </>
+  )
+}
 
 const Content = (props) => {
   return(
     <>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part part={props.part1}
+            exercise={props.exercises1}
+      />
+      <Part part={props.part2}
+            exercise={props.exercises2}
+      />
+      <Part part={props.part3}
+            exercise={props.exercises3}
+      />
     </>
   )
 }
@@ -37,7 +45,7 @@ const Total = (props) => {
 
 
 const App = () => {
-   const course = 'Half Stack application development'
+  const course = 'Half Stack application development'
   const part1 = {
     name: 'Fundamentals of React',
     exercises: 10
