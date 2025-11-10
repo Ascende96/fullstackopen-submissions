4560031aa9a98a75c3466dce5befb17e33aec745
Mariam arkiza <arkizamariam96@gmail.com>
2025-11-10 17:55:50 +0100

    Exercise1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -11,11 +11,32 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [avg, setAvg] = useState(0)
+  const [perc, setPerc] = useState(0)
 
-  const handleGood = () => setGood(good+1)
-  const handleNeutral = () => setNeutral(neutral+1)
-  const handleBad = () => setBad(bad+1)
+  const handleGood = () =>{
+    const updGood = good+1
+    const total = updGood + neutral + bad
+    setGood(updGood)
+    setAvg((updGood-bad)/total)
+    setPerc(100*updGood/total)
+  } 
+  const handleNeutral = () => {
+    const updNeutral= neutral+1
+    const total = good + updNeutral + bad
+    setNeutral(updNeutral)
+    setAvg((good-bad)/total)
+    setPerc(100*good/total)
 
+  }
+  const handleBad = () => {
+    const updBad = bad+1
+    const total = good + neutral + updBad
+    setBad(updBad)
+    setAvg((good-updBad)/total)
+    setPerc(100*good/total)
+
+  }
   return (
     <div>
       <h1>give feedback</h1>
@@ -26,6 +47,8 @@ const App = () => {
       <p>good {good}</p> 
       <p>neutral {neutral}</p> 
       <p>bad {bad}</p> 
+      <p>average {avg}</p>
+      <p>positive {perc}%</p>
     </div>
   )
 }
